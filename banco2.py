7e7ef9412310db89aa8235cd3f42d1f9605ed669
fellypemouranasci@gmail.com <fellypemouranasci@gmail.com>
2025-04-19 19:16:52 -0300

    novas funçoes

--- banco2.py
+++ banco2.py
@@ -1,3 +1,5 @@
+import os
+
 class conta:
     def __init__(self, saldo, numero_conta, agencia, historico):
         self.saldo = saldo
@@ -22,27 +24,55 @@ class PessoaFisica(Cliente):
         self.cpf = cpf
         self.nome = nome 
         self.data_Nascimento = data_Nascimento
+    def __str__(self):
+        return f"CPF:{self.cpf}\nNome:{self.nome}\nData de Nascimento:{self.data_Nascimento}"
 
-def CriarUsuario():
+def CriarUsuario(clientes):
     while 1:
         try:
-            cpf = int(input("Digite o numero do Seu CPF"))
+            cpf = int(input("Digite o seu CPF"))
+            if cpf_existe(clientes, cpf):
+                print("Este CPF já está cadastrado!")
+                input("Pressione Enter para tentar novamente...")
+                limpar_tela()
+                continue
+
             nome = input("Digite o seu nome")
             dt_nascimento = input("Digite sua Data de Nascimento")
 
             cliente = PessoaFisica(cpf , nome , dt_nascimento)
-            clientes = []
             clientes.append(cliente)
             break
         except ValueError:
             print("Digite um Valor Valido!!") 
 
+def cpf_existe(clientes, cpf):
+    for cliente in clientes:
+        if cliente.cpf == cpf:
+            return True
+    return False
+
+
+def ListarUsuarios(clientes):
+    for cliente in clientes:
+        print("------------------------")
+        print(cliente)
+        print("------------------------\n")
+    input("Pressione Enter para continuar")
+
+def limpar_tela():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
 def main(): 
+    clientes = []
     while 1:
-        Entrada = int(input("Bem Vindo!!\n[1]Criar Usuario\n[2]Criar Conta\n[3]Saque\n[4]Deposito\n[5]Sair"))
+        Entrada = int(input("Bem Vindo!!\n[1]Criar Usuario\n[2]Criar Conta\n[3]Saque\n[4]Deposito\n[5]Listar Usuarios\n[6]Sair"))
 
         if Entrada == 1:
-            CriarUsuario()
+            limpar_tela()
+            CriarUsuario(clientes)
+            limpar_tela()
         elif Entrada == 2:
             pass
         elif Entrada == 3:
@@ -50,7 +80,9 @@ def main():
         elif Entrada == 4:
             pass
         elif Entrada == 5:
-            pass
+            limpar_tela()
+            ListarUsuarios(clientes)
+            limpar_tela()
         else:
             print("Digite um Valor valido!!")
 main()
